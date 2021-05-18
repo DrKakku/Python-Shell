f3c44b3272b6e8ae864c190dcd38e76ce84c0105
Hitesh Goyal <55281094+indianeagle4599@users.noreply.github.com>
2021-05-18 12:40:47 +0530

    New commands and features
    Added new commands and created command modes for a few of them.

--- PyShell.py
+++ PyShell.py
@@ -4,6 +4,7 @@ import subprocess
 
 curpath = os.getcwd()
 
+
 def main():
     isPy = False
     while 1:
@@ -12,10 +13,12 @@ def main():
             command = input(f'PYSH | {curpath} | $ ')
             command = command.strip()
             if command == 'exit':
-                print('\n\nPYSH : Thank you for using this Python Shell. Good Bye :)\n\n')
+                print(
+                    '\n\nPYSH : Thank you for using this PyCommander. Good Bye :)\n\n')
                 break
             elif command == 'help':
-                print('\nPYSH : This is a simple Python-based Shell.\n')
+                run_help()
+
             elif command.lower() == 'py on':
                 isPy = True
             else:
@@ -41,57 +44,18 @@ def main():
 def run_cmd(command):
 
     if not runManual(command):
-
         try:
-
-            if '|' in command:
-                # save for restoring later on
-                s_in, s_out = (0, 0)
-                s_in = os.dup(0)
-                s_out = os.dup(1)
-
-                # first command takes commandut from stdin
-                fdin = os.dup(s_in)
-
-                # iterate over all the commands that are piped
-                for cmd in command.split('|'):
-                    # fdin will be stdin if it's the first iteration
-                    # and the readable end of the pipe if not.
-                    os.dup2(fdin, 0)
-                    os.close(fdin)
-
-                    # restore stdout if this is the last command
-                    if cmd == command.split('|')[-1]:
-                        fdout = os.dup(s_out)
-                    else:
-                        fdin, fdout = os.pipe()
-
-                    # redirect stdout to pipe
-                    os.dup2(fdout, 1)
-                    os.close(fdout)
-
-                    try:
-                        subprocess.run(cmd.strip().split())
-                    except Exception:
-                        print(f"PYSH : Command '{cmd.strip()}' not found")
-
-                # restore stdout and stdin
-                os.dup2(s_in, 0)
-                os.dup2(s_out, 1)
-                os.close(s_in)
-                os.close(s_out)
-            
-            else:
-                subprocess.run(command.split(' '))
+            subprocess.run(command.split(' '))
 
         except Exception:
             print(f"PYSH : Command '{command}' not found")
+
 
 def runManual(command):
     curpath = os.getcwd()
     if command == 'pwd' or command == 'showpath':
         return show_path()
-    
+
     elif command == 'ls' or command == 'list':
         return show_list()
 
@@ -113,7 +77,120 @@ def runManual(command):
     elif command[:3] == 'rm ':
         return delete_item(os.path.join(curpath, command[3:]), command[3:])
 
+    elif command[:7] == 'rename ':
+        cmd = command.split(' ')
+        return rename_item(os.path.join(curpath, cmd[1]), cmd[1], os.path.join(curpath, cmd[2]))
+
+    elif command[:3] == 'cp ':
+        cmd = command.split(' ')
+        return copy_item(os.path.join(curpath, cmd[1]), cmd[1], os.path.join(curpath, cmd[2]))
+    
+    elif command[:3] == 'mv ':
+        cmd = command.split(' ')
+        return move_item(os.path.join(curpath, cmd[1]), cmd[1], os.path.join(curpath, cmd[2]))
+
+    elif command[:5] == 'link ':
+        cmd = command[5:].split(' ')
+        return make_Link(os.path.join(curpath, cmd[0]), os.path.join(curpath, cmd[1]))
+
+    elif command[:6] == 'merge ':
+        cmd = command[6:].split(' ')
+        # Merge all directories
+        if cmd[0] == '-a':
+            # Merge and delete
+            if cmd[-1] == '-d':
+                return merge_AllDirs(os.path.join(curpath, cmd[1]), True)
+            # Merge and keep original
+            else:
+                return merge_AllDirs(os.path.join(curpath, cmd[1]), False)
+        # Merge specific directories
+        else:
+            if cmd[-1] == '-d':
+                return merge_GivenDirs(cmd[:-1], curpath, os.path.join(curpath, cmd[-2]), True)
+            else:
+                return merge_GivenDirs(cmd, curpath, os.path.join(curpath, cmd[-1]), False)
+
     return False
+
+# +---------------------------------------+
+# | INDIVIDUAL FUNCTIONS FOR ALL COMMANDS |
+# +---------------------------------------+
+
+def merge_GivenDirs(lst, currentPath, targetPath, deleteOriginal=False):
+    try:
+        for i in lst[:-1]:
+            currentfilePath = os.path.join(currentPath, i)
+            shutil.copytree(currentfilePath, os.path.join(targetPath, i))
+            if deleteOriginal:
+                toDelete = currentfilePath
+                if os.path.isfile(toDelete):
+                    os.remove(toDelete)
+                elif os.path.isdir(toDelete):
+                    try:
+                        os.rmdir(toDelete)
+                    except:
+                        shutil.rmtree(toDelete)
+    except Exception as exc:
+        print(f'PYSH : {exc}')
+    return True
+
+
+def merge_AllDirs(targetPath, deleteOriginal = False):
+    try:
+        lst = os.listdir()
+        target = os.path.basename(os.path.normpath(targetPath))
+        for i in lst:
+            if i != target:
+                currentfilePath = os.path.join(os.getcwd(), i)
+                shutil.copytree(currentfilePath, os.path.join(targetPath, i))
+                if deleteOriginal:
+                    toDelete = currentfilePath
+                    if os.path.isfile(toDelete):
+                        os.remove(toDelete)
+                    elif os.path.isdir(toDelete):
+                        try:
+                            os.rmdir(toDelete)
+                        except:
+                            shutil.rmtree(toDelete)
+    except Exception as exc:
+        print(f'PYSH : {exc}')
+    return True
+
+
+def make_Link(srcfilePath, destfilePath):
+    try:
+        os.symlink(srcfilePath, destfilePath)
+    except Exception as exc:
+        print(f'PYSH : {exc}')
+    return True
+
+
+def move_item(currentfilePath, currentfilename, newfilePath):
+    try:
+        shutil.move(currentfilePath, newfilePath)
+    except Exception as exc:
+        print(f'PYSH : {exc}')
+    return True
+
+
+def copy_item(currentfilePath, currentfilename, newfilePath):
+    try:
+        shutil.copy(currentfilePath, newfilePath)
+    except:
+        try:
+            shutil.copytree(currentfilePath, os.path.join(newfilePath, currentfilename))
+        except Exception as exc:
+            print(f'PYSH : {exc}')
+    return True
+
+
+def rename_item(oldfilePath, oldfilename, newfilePath):
+    try:
+        os.rename(oldfilePath, newfilePath)
+    except:
+        print(f"PYSH : File '{oldfilename}' not found")
+    return True
+
 
 def delete_item(filepath, filename):
     try:
@@ -130,7 +207,7 @@ def delete_item(filepath, filename):
                     try:
                         shutil.rmtree(filepath)
                     except Exception as exc:
-                        print(f'PYSH : {exc}')            
+                        print(f'PYSH : {exc}')
 
     except Exception as exc:
         print(f'PYSH : {exc}')
@@ -172,8 +249,9 @@ def edit_file(filename):
     print('\n\n')
     return True
 
+
 def read_file(filepath, filename):
-    
+
     try:
         reader = open(filepath, "r+")
         old = reader.read()
@@ -182,7 +260,7 @@ def read_file(filepath, filename):
         for i in broken:
             if len(i) > maxl:
                 maxl = len(i)
-        
+
         print()
         while dash in range(maxl+2):
             print('-', end='')
@@ -195,7 +273,7 @@ def read_file(filepath, filename):
             print('-', end='')
             dash += 1
         print()
-            
+
         reader.close()
 
     except:
@@ -203,17 +281,25 @@ def read_file(filepath, filename):
 
     return True
 
+
 def make_file(filename):
     open(filename, 'a').close()
     return True
 
+
 def make_dir(dirName):
-    os.mkdir(dirName)
+    dirName = dirName.split(' ')
+    mode = dirName[-1]
+    os.mkdir(dirName[0])
+    if mode == '-cd':
+        change_dir(dirName[0])
     return True
+
 
 def show_path():
     print(f'PYSH : {os.getcwd()}')
     return True
+
 
 def show_list():
     print(f'PYSH : ')
@@ -223,11 +309,161 @@ def show_list():
     print()
     return True
 
+
 def change_dir(path):
+    path = path.strip().split(' ')
+    mode = path[-1]
     try:
-        os.chdir(os.path.abspath(path))
+        os.chdir(os.path.abspath(path[0]))
+        if mode == '-ls':
+            show_list()
+
     except:
-        print(f'PYSH | {curpath} | cd: No such file or directory: {path}')
+        print(f'PYSH | {curpath} | cd: No such file or directory: {path[0]}')
+    return True
+
+
+def run_help():
+
+    manual = {
+        'exit':
+        '''
+        Usage: $ exit
+        Function: Exits (quits) pycommander.
+        ''',
+
+        'pwd':
+        '''
+        Usage: $ pwd OR $ showpath
+        Function: Prints the current working directory (path to current directory).
+        ''',
+
+        'showpath':
+        '''
+        Usage: $ pwd OR $ showpath
+        Function: Prints the current working directory (path to current directory).
+        ''',
+
+        'ls':
+        '''
+        Usage: $ ls OR $ list
+        Function: Prints the contents of the current working directory.
+        ''',
+
+        'list':
+        '''
+        Usage: $ ls OR $ list
+        Function: Prints the contents of the current working directory.
+        ''',
+
+        'cd':
+        '''
+        Usage: $ cd {name/path of directory}
+        Function: Navigate to target directory from current directory.
+        
+        Mode: -ls
+        Mode Usage: $ cd {name/path of directory} -ls
+        Mode Function: Print contents of new directory after navigating.
+        ''',
+
+        'mkdir':
+        '''
+        Usage: $ mkdir {name/path of new directory}
+        Function: Create a new directory with the required name, having the required path.
+
+        Mode: -cd
+        Mode Usage: $ mkdir {name/path of new directory} -cd
+        Mode Function: Move to new directory after creating it.
+        ''',
+
+        'create':
+        '''
+        Usage: $ create {file name}.{file extension}
+        Function: Used to create a blank file.
+        ''',
+
+        'edit':
+        '''
+        Usage: $ edit {file name}.{file extension} OR $ edit {file location}
+        Function: Used to edit an existing file. Creates a blank file for editting if one doesn't exist.
+        ''',
+
+        'read':
+        '''
+        Usage: $ read {file name}.{file extension} OR $ read {file location}
+        Function: Used to merely read an existing file. Does not work if file doesn't exist.
+        ''',
+
+        'rename':
+        '''
+        Usage: $ rename {old file name} {new file name}
+        Function: Changes the name of a file from the old one to the new one.
+        ''',
+
+        'cp':
+        '''
+        Usage: cp {file name} {location of target directory}
+        Function: Simply copies the file or directory from the current location to the new location. Target location must be a directory.
+        ''',
+
+        'link':
+        '''
+        Usage: $ link {source location} {new location}
+        Function: Creates a hard link of the given source in the new link location.
+        ''',
+
+        'rm':
+        '''
+        Usage: $ rm {name of file/directory to remove} OR $ rm {location of file/directory to remove}
+        Function: Deletes a file or directory permanently. Asks confirmation for non-empty directories. Does not work if file doesn't exist.
+        ''',
+
+        'mv':
+        '''
+        Usage: $ mv {old location} {new location}
+        Function: Moves files and directories from the old location to the new location.
+        ''',
+
+        'merge':
+        '''
+        Usage: $ merge {dir location 1} {dir location 2} ... {dir location n} {target directory}
+        Function: Merges all listed directories into the target directory.
+
+        Mode: -a
+        Mode Usage: $ merge -a {target directory}
+        Mode Function: Merges all directories in the current location into the target directory.
+
+        Mode: -d
+        Mode Usage: $ merge {dir location 1} {dir location 2} ... {dir location n} {target directory} -d
+        Mode Function: Delete the original directories upon merging.
+
+        Mode: -a -d
+        Mode Usage: $ merge -a {target directory} -d
+        Mode Function: Merges all directories in the current location into the target directory and delete the original directories upon merging.
+        ''',
+    }
+
+    command_list = list(manual.keys())
+
+    cmd = 'print'
+    while cmd != 'quit':
+        if cmd == 'print':
+            print('\n')
+            for i in range(len(command_list)):
+                print(f'\t- {command_list[i]}')
+            cmd = ''
+
+        print(
+            "\n\nPYHE : Enter 'quit' to exit help page and 'print' to see the list of commands again")
+        cmd = input('Enter the command of your choice: ')
+
+        if cmd != 'quit' and cmd != 'print':
+            try:
+                print('PYHE :')
+                print(manual[cmd])
+            except:
+                print('PYHE: Command not found. Please check and try again.')
+
     return True
 
 
